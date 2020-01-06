3fe49799768f465e6bf4203ac194f2ecf44aa544
Apple <apple@Apples-MacBook-Pro.local>
2020-01-06 22:08:44 +0545

    test report url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { DigiiService } from './services/protractor.service';
+import { Router } from '@angular/router';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +12,9 @@ export class AppComponent implements OnInit{
   data: any;
 
 constructor(
-  private service: DigiiService
+  private service: DigiiService,
+  public router: Router,
+  @Inject(DOCUMENT) private document: Document
 ) { }
 ngOnInit() {
 }
@@ -21,5 +25,9 @@ onSubmit() {
     console.log(res);
     alert('SUCCESS !!');
   })
+}
+
+testReport(){
+  this.document.location.href = 'http://52.62.45.84/ProtractorTestReport.html';
 }
 }
